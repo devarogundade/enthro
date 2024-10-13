ed186dfddb92b27fae2340ef5035020957201537
devarogundade <devarogundade@gmail.com>
2024-10-13 16:09:49 +0100

    update radio icon

--- ui/src/components/icons/RadioToggleIcon.vue
+++ ui/src/components/icons/RadioToggleIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="9" cy="9" r="4.5" :fill="props.active ? 'var(--bg-darker)' : ''" />
+        <circle cx="9" cy="9" r="4.5" :fill="props.active ? 'var(--primary)' : ''" />
         <circle cx="9" cy="9" r="8.25" :stroke="props.active ? 'var(--primary-light)' : 'var(--bg-darker)'"
             stroke-width="1.5" />
     </svg>
